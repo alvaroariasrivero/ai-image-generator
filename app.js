c204fa713ee2d95d3665905f9ea85d1df2875bfa
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2024-07-08 23:18:50 +0200

    request done

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 const submitForm = document.getElementById('api-request-form');
+const textInput = document.getElementById('request-container');
+// const Api_KEY = "";
 
-submitForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+submitForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      "Authorization": `Bearer ${Api_KEY}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "prompt": textInput.value,
+      "model": "dall-e-2",
+      "n": 1,
+      "size": "1024x1024"
+    }) 
+  };
+  try {
+    const response = await fetch('https://api.openai.com/v1/images/generations', requestOptions);
+    const data = await response.json();
+    console.log('esto es data', data);
+  } catch (error) {
+    console.error(error);
+  }
 });
