f8c43e07a4cbe535b7804b7b069146a47a0ceba1
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2024-07-12 12:34:28 +0200

    starrai integration, cors issue

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const submitForm = document.getElementById('api-request-form');
 const textInput = document.getElementById('request-container');
-// const Api_KEY = "";
+const Api_KEY = "";
+// const apiUrl = 'https://api.starryai.com/user/';
 
 submitForm.addEventListener('submit', async (e) => {
   e.preventDefault();
@@ -25,3 +26,34 @@ submitForm.addEventListener('submit', async (e) => {
     console.error(error);
   }
 });
+
+// STARRAI API CORS ISSUE
+// submitForm.addEventListener('submit', async (e) => {
+//   e.preventDefault();
+//   const options = {
+//     method: 'POST',
+//     headers: {
+//       accept: 'application/json',
+//       'content-type': 'application/json',
+//       'X-API-Key': 'q5PSEA0Rfwg7bPt2cP2QSsd0AfXQGg',
+//       'access-control-allow-credentials':	'*'
+//     },
+//     body: JSON.stringify({
+//       model: 'lyra',
+//       aspectRatio: 'square',
+//       highResolution: false,
+//       images: 4,
+//       steps: 20,
+//       initialImageMode: 'color',
+//       prompt: 'dancing dog'
+//     })
+//   };
+//   try {
+//     fetch('https://api.starryai.com/creations/', options)
+//       .then(response => response.json())
+//       .then(response => console.log(response))
+//       .catch(err => console.error(err));
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
